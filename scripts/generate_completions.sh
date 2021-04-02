20ec6a76d3c9ec6b529bfffb98fc964043abb99f
Todd Morse <todd@morsecodist.io>
2021-04-02 16:29:41 -0400

    Package Completions (#5)

---
+++ scripts/generate_completions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+$1 completion bash > ./bash_completion
+$1 completion zsh > ./zsh_completion
+$1 completion fish > ./fish_completion
+$1 completion powershell > ./powershell_completion
