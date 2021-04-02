81207842634c693b673b517d96fc9c2bd6d5a1d5
morsecodist <todd@morsecodist.io>
2021-04-02 14:15:36 -0400

    cleanup

--- scripts/generate_completions.sh
+++ scripts/generate_completions.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 $1 completion bash > ./bash_completions
+$1 completion zsh > ./zsh_completions
+$1 completion fish > ./fish_completions
+$1 completion powershell > ./powershell_completions
