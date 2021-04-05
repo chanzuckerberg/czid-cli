3e96b4ae40a43fd648cad0b4640aeb7b14683b59
morsecodist <todd@morsecodist.io>
2021-04-05 12:01:33 -0400

    remove dir

--- scripts/generate_completions.sh
+++ scripts/generate_completions.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-mkdir $1/completions
-
-$2 completion bash > $1/completions/bash
-$2 completion zsh > $1/completions/zsh
-$2 completion fish > $1/completions/fish
-$2 completion powershell > $1/completions/powershell
+$1 completion bash > bash_completion
+$1 completion zsh > zsh_completion
+$1 completion fish > fish_completion
+$1 completion powershell > powershell_completion
