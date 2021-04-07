faa95fb14d83121ecdfbc253bdf93fa9d956dc61
morsecodist <todd@morsecodist.io>
2021-04-07 16:47:21 -0400

    add test

---
+++ cmd/shortReadMNGS/uploadSamples_test.go
@@ -0,0 +1,27 @@
+package shortReadMNGS
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/spf13/viper"
+)
+
+func TestWithoutProjectID(t *testing.T) {
+	viper.Set("ACCEPTED_USER_AGREEMENT", "Y")
+	ShortReadMNGSCmd.PersistentFlags().Bool("verbose", false, "")
+	b := bytes.NewBufferString("")
+	e := bytes.NewBufferString("")
+	ShortReadMNGSCmd.SetOut(b)
+	ShortReadMNGSCmd.SetErr(e)
+	ShortReadMNGSCmd.SetArgs([]string{"upload-samples"})
+	ShortReadMNGSCmd.Execute()
+	errOut, err := ioutil.ReadAll(e)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(errOut) != "Error: missing required argument: project\n" {
+		t.Fatalf("expected a missing project error but error was: %s", string(errOut))
+	}
+}
