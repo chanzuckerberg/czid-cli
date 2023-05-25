8df448ffa8c8616846cb2f53c6a196fbeeaefd32
lvreynoso <lreynoso@chanzuckerberg.com>
2023-05-25 10:40:58 -0700

    Add file_type param to createSamplesReqInputFile
    * create type FileType string with string constants
    * assign file type based on sampleOptions

--- pkg/czid/uploadSamples.go
+++ pkg/czid/uploadSamples.go
@@ -9,6 +9,7 @@ import (
 )
 
 type createSamplesReqInputFile struct {
+	FileType	 FileType `json:"file_type"`
 	Name         string `json:"name"`
 	Parts        string `json:"parts"`
 	Source       string `json:"source"`
@@ -54,6 +55,13 @@ type UploadInfo struct {
 	MultipartUploadId *string `json:"multipart_upload_id"`
 	S3Path            string  `json:"s3_path"`
 }
+
+type FileType string
+const (
+	FASTQFileType FileType = "fastq"
+	PrimerBedFileType FileType = "primer_bed"
+	ReferenceAccessionFileType FileType = "ref_seq"
+)
 
 // CreateSamples creates samples on the back end and returns the necessary information to upload their files
 func (c *Client) CreateSamples(
@@ -109,22 +117,28 @@ func (c *Client) CreateSamples(
 			sample.ClearLabs = &sampleOptions.ClearLabs
 		}
 
+		var filetype FileType
+
 		if sampleOptions.ReferenceAccession != "" {
 			sample.ReferenceAccession = &sampleOptions.ReferenceAccession
+			filetype = ReferenceAccessionFileType
 		}
 
 		if sampleOptions.ReferenceFasta != "" {
 			referenceFasta := filepath.Base(sampleOptions.ReferenceFasta)
 			sample.ReferenceFasta = &referenceFasta
+			filetype = FASTQFileType
 		}
 
 		if sampleOptions.PrimerBed != "" {
 			primerBed := filepath.Base(sampleOptions.PrimerBed)
 			sample.PrimerBed = &primerBed
+			filetype = PrimerBedFileType
 		}
 
 		for i, filename := range filenames {
 			sample.InputFileAttributes[i] = createSamplesReqInputFile{
+				FileType:	  filetype,
 				Name:         filepath.Base(filename),
 				Parts:        filepath.Base(filename),
 				Source:       filepath.Base(filename),
